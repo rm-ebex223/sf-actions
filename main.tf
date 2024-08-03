c66f30e8390db59a11fa4f918f115890cff8c578
Ryan Miller <124627804+rm-ebex223@users.noreply.github.com>
2024-08-03 10:46:11 -0400

    break stuff

--- main.tf
+++ main.tf
@@ -34,8 +34,8 @@ resource "snowflake_schema" "demo_schema" {
 #   comment = "Database for Snowflake Terraform demo"
 # }
 
-# resource "snowflake_schema" "more_demo_schema" {
-#   database = snowflake_database.demo_db_2.name
-#   name     = "MORE_DEMO_SCHEMA"
-#   comment  = "Schema for Snowflake Terraform demo"
-# }
+resource "snowflake_schema" "more_demo_schema" {
+  database = snowflake_database.demo_db_2.name
+  name     = "MORE_DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
+}
