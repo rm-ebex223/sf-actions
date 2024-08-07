f607b62b427a2952c892bcefafa3470c8fa3703b
Ryan Miller <124627804+rm-ebex223@users.noreply.github.com>
2024-08-07 19:24:21 -0400

    ....

--- main.tf
+++ main.tf
@@ -30,10 +30,10 @@ resource "snowflake_database" "demo_db" {
 #   comment  = "Schema for Snowflake Terraform demo"
 # }
 
-resource "snowflake_database" "demo_db_2" {
-  name    = "ANOTHER_DEMO_DB"
-  comment = "Database for Snowflake Terraform demo"
-}
+# resource "snowflake_database" "demo_db_2" {
+#   name    = "ANOTHER_DEMO_DB"
+#   comment = "Database for Snowflake Terraform demo"
+# }
 
 # resource "snowflake_schema" "more_demo_schema" {
 #   database = snowflake_database.demo_db_2.name
