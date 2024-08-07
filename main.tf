fa25f9b1a6d99c0ce15138797b009df0f93694d3
Ryan Miller <124627804+rm-ebex223@users.noreply.github.com>
2024-08-07 18:44:49 -0400

    test-dev-again

--- main.tf
+++ main.tf
@@ -30,13 +30,13 @@ resource "snowflake_schema" "demo_schema" {
   comment  = "Schema for Snowflake Terraform demo"
 }
 
-resource "snowflake_database" "demo_db_2" {
-  name    = "ANOTHER_DEMO_DB"
-  comment = "Database for Snowflake Terraform demo"
-}
+# resource "snowflake_database" "demo_db_2" {
+#   name    = "ANOTHER_DEMO_DB"
+#   comment = "Database for Snowflake Terraform demo"
+# }
 
-resource "snowflake_schema" "more_demo_schema" {
-  database = snowflake_database.demo_db_2.name
-  name     = "MORE_DEMO_SCHEMA"
-  comment  = "Schema for Snowflake Terraform demo"
-}
+# resource "snowflake_schema" "more_demo_schema" {
+#   database = snowflake_database.demo_db_2.name
+#   name     = "MORE_DEMO_SCHEMA"
+#   comment  = "Schema for Snowflake Terraform demo"
+# }
