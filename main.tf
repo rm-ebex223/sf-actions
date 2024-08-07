cd75f7882198f803f3954088543fe6a5bc2f3519
Ryan Miller <124627804+rm-ebex223@users.noreply.github.com>
2024-08-07 19:40:56 -0400

    ....

--- main.tf
+++ main.tf
@@ -19,10 +19,10 @@ terraform {
 provider "snowflake" {
 }
 
-resource "snowflake_database" "demo_db" {
-  name    = "DB_DB"
-  comment = "Database for Snowflake Terraform demo"
-}
+# resource "snowflake_database" "demo_db" {
+#   name    = "DB_DB"
+#   comment = "Database for Snowflake Terraform demo"
+# }
 
 # resource "snowflake_schema" "demo_schema" {
 #   database = snowflake_database.demo_db.name
@@ -30,10 +30,10 @@ resource "snowflake_database" "demo_db" {
 #   comment  = "Schema for Snowflake Terraform demo"
 # }
 
-resource "snowflake_database" "demo_db_2" {
-  name    = "ANOTHER_DEMO_DB"
-  comment = "Database for Snowflake Terraform demo"
-}
+# resource "snowflake_database" "demo_db_2" {
+#   name    = "ANOTHER_DEMO_DB"
+#   comment = "Database for Snowflake Terraform demo"
+# }
 
 # resource "snowflake_schema" "more_demo_schema" {
 #   database = snowflake_database.demo_db_2.name
@@ -41,7 +41,7 @@ resource "snowflake_database" "demo_db_2" {
 #   comment  = "Schema for Snowflake Terraform demo"
 # }
 
-resource "snowflake_database" "demo_db_2" {
-  name    = "ANOTHER_DEMO_DB"
-  comment = "Database for Snowflake Terraform demo"
-}
+# resource "snowflake_database" "demo_db_2" {
+#   name    = "ANOTHER_DEMO_DB"
+#   comment = "Database for Snowflake Terraform demo"
+# }
