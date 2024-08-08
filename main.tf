7220ea0c5a352266bb358192a090097f878f2eef
rm-ebex223 <124627804+rm-ebex223@users.noreply.github.com>
2024-08-08 00:40:19 -0400

    Develop (#77)

--- main.tf
+++ main.tf
@@ -19,16 +19,16 @@ terraform {
 provider "snowflake" {
 }
 
-resource "snowflake_database" "demo_db" {
-  name    = "DB_DB"
-  comment = "Database for Snowflake Terraform demo"
-}
+# resource "snowflake_database" "demo_db" {
+#   name    = "DB_DB"
+#   comment = "Database for Snowflake Terraform demo"
+# }
 
-resource "snowflake_schema" "demo_schema" {
-  database = snowflake_database.demo_db.name
-  name     = "DEMO_SCHEMA"
-  comment  = "Schema for Snowflake Terraform demo"
-}
+# resource "snowflake_schema" "demo_schema" {
+#   database = snowflake_database.demo_db.name
+#   name     = "DEMO_SCHEMA"
+#   comment  = "Schema for Snowflake Terraform demo"
+# }
 
 # resource "snowflake_database" "demo_db_2" {
 #   name    = "ANOTHER_DEMO_DB"
@@ -37,5 +37,10 @@ resource "snowflake_schema" "demo_schema" {
 
 # resource "snowflake_database" "demo_db_3" {
 #   name    = "MORE_DEMO_DB"
+#   comment = "Database for Snowflake Terraform demo"
+# }
+
+# resource "snowflake_database" "boop" {
+#   name    = "BOOP"
 #   comment = "Database for Snowflake Terraform demo"
 # }
