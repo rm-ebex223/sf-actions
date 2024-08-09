118aadf99ddff753334c712d890fc1c013e01d2c
Ryan Miller <124627804+rm-ebex223@users.noreply.github.com>
2024-08-09 10:51:57 -0400

    ....

--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
-variable "snowflake_account" {
-  type        = string
-  description = "Snowflake account name"
-}
+# variable "snowflake_account" {
+#   type        = string
+#   description = "Snowflake account name"
+# }
 
-variable "snowflake_username" {
-  type        = string
-  description = "Snowflake account username"
-}
+# variable "snowflake_username" {
+#   type        = string
+#   description = "Snowflake account username"
+# }
 
-variable "snowflake_password" {
-  type        = string
-  description = "Snowflake account password"
-}
+# variable "snowflake_password" {
+#   type        = string
+#   description = "Snowflake account password"
+# }
 
-variable "snowflake_role" {
-  type        = string
-  description = "Snowflake account login role"
-}
+# variable "snowflake_role" {
+#   type        = string
+#   description = "Snowflake account login role"
+# }
 
 
