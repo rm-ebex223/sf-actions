9909a0bb55415be345be5314a5c6cc32601e94c5
Ryan Miller <124627804+rm-ebex223@users.noreply.github.com>
2024-08-09 11:18:45 -0400

    ....

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-# module "LIQUIBASE_TEST" {
-#   source        = "./modules/databases"
-#   database_name = "LIQUIBASE_TEST"
-#   # database_grants = {
-#   #   #SYSADMIN = ["USAGE"]
-#   # }
-#   #schema_info = [
-#   #{ schema_name = "TEST_STUFF" }
-#   #{ schema_name = "MORE_TEST" }
-#   #]
-#   #add_liquibase = false
-# }
+module "LIQUIBASE_TEST" {
+  source        = "./modules/databases"
+  database_name = "LIQUIBASE_TEST"
+  # database_grants = {
+  #   #SYSADMIN = ["USAGE"]
+  # }
+  #schema_info = [
+  #{ schema_name = "TEST_STUFF" }
+  #{ schema_name = "MORE_TEST" }
+  #]
+  #add_liquibase = false
+}
 
 
 # module "LIQUIBASE_WH" {
